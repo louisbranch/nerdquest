8ae2989ab3fa999015878a77a590dee84ed731b5
Luiz Branco <artfikan@gmail.com>
2012-05-27 12:07:58 -0300

    Created spec for integration with Koala gem

--- spec/lib/authentication_spec.rb
+++ spec/lib/authentication_spec.rb
@@ -7,6 +7,13 @@ module NerdQuest
 
     let(:auth) {Authentication.new('123')}
 
+    it "requests the Koala Facebook API" do
+      api = double('koala')
+      Koala::Facebook::OAuth.stub(:new).and_return(api)
+      api.should_receive(:parse_signed_request).with('123')
+      Authentication.api('123')
+    end
+
     it "parses the signed request when instanciated" do
       Authentication.should_receive(:api)
       auth
